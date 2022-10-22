a651814bfd21c4ada1466af33a37051f0d0b6069
João Pablo <jp.pablolima@gmail.com>
2022-10-22 16:29:52 -0300

    apache2 installation with tefttaform

--- ec2/main.tf
+++ ec2/main.tf
@@ -10,15 +10,33 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
-  key_name = "devops"
+  key_name      = "devops"
+  user_data     = <<-EOF
+      #! /bin/bash
+      sudo apt-get update
+      sudo apt-get install -y apache2
+      sudo systemctl start apache2
+      sudo systemctl enable apache2
+      echo "Deployed by Terraform!" | sudo tee /var/www/html/index.html
+    EOF
 
   tags = {
-    Name = "Ubuntu Server"
+    Name = "Ubuntu Server2"
+  }
+}
+
+resource "aws_security_group" "app_server" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
