0dc9b11c05e87ca49b23d805b3e3b4311e6b810b
João Pablo <jp.pablolima@gmail.com>
2023-04-06 12:24:37 -0300

    data terraform instance ec2

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,5 +1,16 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] 
+  
+}
 resource "aws_instance" "jenkins" {
-    ami = "ami-007855ac798b5175e"
+    ami = data.aws_ami.ubuntu.id
     instance_type = "t2.micro"
 
     tags = {
